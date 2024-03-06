01dba84621f77fd1b75151588cbad174442215ff
Shmithy16 <ben.arrowsmith17@gmail.com>
2024-03-06 14:55:48 +0000

    finished?

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ struct tweet{
     GeoLocation location;
 };
 void display_tweet_by_value(tweet tweet1){
+    cout << "Display tweets by value" << endl;
     cout << string(15,'*') <<tweet1.id << string(15,'*') <<endl;
     cout << tweet1.msg << endl;
     cout << tweet1.author << "\t" << tweet1.timestamp <<endl;
@@ -21,6 +22,7 @@ void display_tweet_by_value(tweet tweet1){
     cout << string(31,'*') << endl;
 }
 void display_tweet_by_reference(const tweet &tweet1){
+    cout << "Display tweets by reference" << endl;
     cout << string(15,'*') <<tweet1.id << string(15,'*') <<endl;
     cout << tweet1.msg << endl;
     cout << tweet1.author << "\t" << tweet1.timestamp <<endl;
@@ -29,12 +31,21 @@ void display_tweet_by_reference(const tweet &tweet1){
 }
 
 void display_tweet_by_pointer(const tweet *tweet1){
+    cout << "Display tweets by pointer" << endl;
     cout << string(15,'*') << tweet1->id << string(15,'*') <<endl;
     cout << tweet1->msg << endl;
     cout << tweet1->author << "\t" << tweet1->timestamp <<endl;
     cout << "(" << tweet1->location.lat << ", " << tweet1->location.lng << ")" <<endl;
     cout << string(31,'*') << endl;
 }
+
+void display_tweets_by_array(const tweet tweetArr[], int size){
+    cout << "Display tweets in array" << endl;
+    for(int i = 0; i< size; i++){
+        display_tweet_by_reference(tweetArr[i]);
+    }
+}
+
 
 int main() {
     tweet tweet1;
@@ -52,14 +63,26 @@ int main() {
     tweet *tweets = new tweet[3];
     tweets[0] = tweet1;
 
-    delete[] tweets;
-    tweets = nullptr;
-
-    tweets[1].msg = "My first tweet";
-    tweets[1].author = "Ben";
-    tweets[1].id = 1;
+    tweets[1].msg = "My second tweet";
+    tweets[1].author = "Ida";
+    tweets[1].id = 2;
     tweets[1].timestamp = 1709130764;
     tweets[1].location.lat = 59.5923569;
     tweets[1].location.lng = 1.326927926;
+
+    tweets[2].msg = "My third tweet";
+    tweets[2].author = "Cam+";
+    tweets[2].id = 3;
+    tweets[2].timestamp = 1709130764;
+    tweets[2].location.lat = 59.5923569;
+    tweets[2].location.lng = 1.326927926;
+
+    delete[] tweets;
+    tweets = nullptr;
+
+    tweet arr[2]{tweets[1],tweets[2]};
+
+    display_tweets_by_array(arr,2);
+
     return 0;
 }
